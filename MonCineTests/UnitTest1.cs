f883d6ecde196ea98b283c127c93f4ed6d8337da
LaiebM <marvin_laieb@hotmail.com>
2022-04-12 13:16:58 -0400

    Ajout du projet de tests unitaires

---
+++ MonCineTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonCine.Data;
+namespace MonCineTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DAL dal = new DAL();
+        }
+
+    }
+}
