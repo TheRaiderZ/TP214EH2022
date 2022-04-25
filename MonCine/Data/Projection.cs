7d7e43e76624df8aeef7e5c48de7303a08e2073e
Tommy Belanger <t.belanger@edu.cegepgarneau.ca>
2022-04-24 20:25:18 -0400

    TommyCommit

---
+++ MonCine/Data/Projection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonCine.Data
+{
+    public class Projection
+    {
+        public Salle salle { get; set; }
+        public DateTime dateDebut { get; set; }
+        public DateTime dateFin { get; set; }
+        public Film film { get; set; }
+
+        public Projection(Salle salle, DateTime dateDebut, DateTime dateFin, Film film)
+        {
+            this.salle = salle;
+            this.dateDebut = dateDebut;
+            this.dateFin = dateFin;
+            this.film = film;
+        }
+
+        public override string ToString()
+        {
+            return dateDebut.ToString();
+        }
+    }
+}
